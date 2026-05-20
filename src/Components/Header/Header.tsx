84cf0bde4cd6cb6d65a5f68755587131fcf1d305
Thiago Pessoa <tpessoa96@gmail.com>
2026-05-20 08:48:36 -0300

    Estilizando

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,13 +1,15 @@
 import SearchBar from "../SearchBar/SearchBar";
 import styles from './style.module.css'
 
-export default function Header(){
-    return(
+export default function Header() {
+    return (
         <div className={styles.div}>
-            <a href="" className={styles.a}>
-                <h2>TrackingWeather</h2>
-            </a>
-            <SearchBar/>
+            <div className={styles.centro}>
+                <a href="" className={styles.a}>
+                    <h2>TrackingWeather</h2>
+                </a>
+                <SearchBar />
+            </div>
         </div>
     )
 }
